3982875b29d8b1b84ca4cb84826a8649ea5abacd
Fabian Pedregosa <fabian@fseoane.net>
2013-01-22 14:20:51 +0100

    Soem fixes

--- rank_one.py
+++ rank_one.py
@@ -2,7 +2,7 @@ import numpy as np
 from scipy import sparse, linalg
 from scipy.sparse import linalg as splinalg
 
-def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, maxiter=1000, verbose=False):
+def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, maxiter=100, verbose=False):
     """
 
     min over u and v:
@@ -34,9 +34,9 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
         else:
             Ky = Kron_v.T.dot(X.T.dot(y - np.dot(Z, w0)))
         u0, info = splinalg.cg(K, Ky, x0=u0, tol=rtol)
+        u0 = u0.reshape((size_u, 1))
 
         # update u
-        u0 = u0.reshape((size_u, 1))
         Kron_u = sparse.kron(sparse.eye(size_v, size_v), u0)
         def K2_matvec(z):
             return Kron_u.T.dot(X.T.dot(X.dot(Kron_u.dot(z)))) + alpha * z
@@ -46,10 +46,11 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
         else:
             Ky = Kron_u.T.dot(X.T.dot(y - np.dot(Z, w0))) + alpha * np.ones(Kron_u.shape[1])
         v0, info = splinalg.cg(K, Ky, x0=v0, tol=rtol)
+        v0 = v0.reshape((size_v, 1))
 
         # update w
         if Z is not None:
-            w0 = linalg.lstsq(Z, y - X.dot(Kron_u.dot(v0)))[0]
+            w0 = linalg.lstsq(Z, y - X.dot(np.dot(u0, v0.T).ravel('F')))[0]
 
         if verbose:
             v0 = v0.reshape((size_v, 1))
