a86f92db11838eab81b4ec7923d0041655f6003f
Fabian Pedregosa <fabian@fseoane.net>
2013-01-23 14:36:37 +0100

    support for rank > 1

--- rank_one.py
+++ rank_one.py
@@ -2,7 +2,8 @@ import numpy as np
 from scipy import sparse, linalg
 from scipy.sparse import linalg as splinalg
 
-def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, maxiter=100, verbose=False):
+
+def rank_one(X, y, alpha, shape_u, Z=None, u0=None, v0=None, rtol=1e-6, maxiter=100, verbose=False):
     """
 
     min over u and v:
@@ -13,10 +14,12 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
     ----------
     """
     #X = splinalg.aslinearoperator(X)
+    assert len(shape_u) == 2
+    shape_v = (X.shape[1] / shape_u[0], shape_u[1]) # TODO: check first dimension is integer
     if u0 is None:
-        u0 = np.random.randn(size_u)
+        u0 = np.random.randn(*shape_u)
     if v0 is None:
-        v0 = np.ones(size_v) # np.random.randn(shape_B[1])
+        v0 = np.ones(shape_v) # np.random.randn(shape_B[1])
     w0 = None
     if Z is not None:
         w0 = np.zeros(Z.shape[1])
@@ -24,8 +27,9 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
     for _ in range(maxiter):
 
         # update v
-        v0 = v0.reshape((size_v, 1))
-        Kron_v = sparse.kron(v0, sparse.eye(size_u, size_u))
+        v0 = v0.reshape(shape_v)
+        size_id = X.shape[1] / shape_v[0]
+        Kron_v = sparse.kron(v0, sparse.eye(size_id, shape_u[0] * shape_u[1] / shape_v[1]))
         def K_matvec(z):
             return Kron_v.T.dot(X.T.dot(X.dot(Kron_v.dot(z))))
         K = splinalg.LinearOperator((Kron_v.shape[1], Kron_v.shape[1]), matvec=K_matvec, dtype=X.dtype)
@@ -33,11 +37,11 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
             Ky = Kron_v.T.dot(X.T.dot(y))
         else:
             Ky = Kron_v.T.dot(X.T.dot(y - np.dot(Z, w0)))
-        u0, info = splinalg.cg(K, Ky, x0=u0, tol=rtol)
-        u0 = u0.reshape((size_u, 1))
+        u0, info = splinalg.cg(K, Ky, x0=u0.ravel(), tol=rtol)
+        u0 = u0.reshape(shape_u, order='F')
 
         # update u
-        Kron_u = sparse.kron(sparse.eye(size_v, size_v), u0)
+        Kron_u = sparse.kron(sparse.eye(X.shape[1] / shape_u[0], shape_v[1] * shape_v[0] / shape_u[1]), u0)
         def K2_matvec(z):
             return Kron_u.T.dot(X.T.dot(X.dot(Kron_u.dot(z)))) + alpha * z
         K = splinalg.LinearOperator((Kron_u.shape[1], Kron_u.shape[1]), matvec=K2_matvec, dtype=X.dtype)
@@ -45,15 +49,17 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
             Ky = Kron_u.T.dot(X.T.dot(y)) + alpha * np.ones(Kron_u.shape[1])
         else:
             Ky = Kron_u.T.dot(X.T.dot(y - np.dot(Z, w0))) + alpha * np.ones(Kron_u.shape[1])
-        v0, info = splinalg.cg(K, Ky, x0=v0, tol=rtol)
-        v0 = v0.reshape((size_v, 1))
+        vt0, info = splinalg.cg(K, Ky, x0=v0.T.ravel(), tol=rtol)
+        vt0 = vt0.reshape((shape_v[1], shape_v[0]), order='F')
+        v0 = vt0.T
 
         # update w
         if Z is not None:
+            # TODO: cache SVD(Z)
             w0 = linalg.lstsq(Z, y - X.dot(np.dot(u0, v0.T).ravel('F')))[0]
 
         if verbose:
-            v0 = v0.reshape((size_v, 1))
+            v0 = v0.reshape(shape_v)
             if Z is None:
                 pobj = np.linalg.norm(y - X.dot(np.dot(u0, v0.T).ravel('F'))) ** 2 + alpha * linalg.norm(v0 - 1) ** 2
             else:
@@ -61,18 +67,18 @@ def rank_one(X, y, alpha, size_u, size_v, Z=None, u0=None, v0=None, rtol=1e-6, m
 
             print('POBJ: %s' % pobj)
     if Z is None:
-        return u0.reshape((size_u, 1)), v0.reshape((size_v, 1))
+        return u0.reshape(shape_u), v0.reshape(shape_v)
     else:
-        return u0.reshape((size_u, 1)), v0.reshape((size_v, 1)), w0
+        return u0.reshape(shape_u), v0.reshape(shape_v), w0
 
 if __name__ == '__main__':
     size_u, size_v = 10, 8
     X = sparse.csr_matrix(np.random.randn(1000, size_u * size_v))
-    Z = np.random.randn(1000, 4)
-    u_true, v_true = np.random.rand(size_u, 1), 1 + .3 * np.random.randn(size_v, 1)
+    Z = np.random.randn(1000, 20)
+    u_true, v_true = np.random.rand(size_u, 2), 1 + .1 * np.random.randn(size_v, 2)
     B = np.dot(u_true, v_true.T)
-    y = X.dot(B.ravel('F')) + .5 * np.random.randn(X.shape[0])
-    u, v, w = rank_one(X, y, 1e-3, size_u, size_v, Z=Z, maxiter=200, verbose=True)
+    y = X.dot(B.ravel('F')) + .3 * np.random.randn(X.shape[0])
+    u, v = rank_one(X, y, .1, (size_u, 2), Z=None, maxiter=500, verbose=True)
 
     import pylab as plt
     plt.matshow(B)
